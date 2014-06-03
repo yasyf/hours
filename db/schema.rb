496e5fc93fbaf9d0d5f1207469d4f44ab652138c
Yasyf Mohamedali <yasyfm@gmail.com>
2014-06-03 03:12:15 -0400

    added paid to entries

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140603024246) do
+ActiveRecord::Schema.define(version: 20140603071055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140603024246) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "paid"
   end
 
 end
