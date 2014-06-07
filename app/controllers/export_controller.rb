c1f27245b150075d55452be8604a25c92968bcf3
Yasyf Mohamedali <yasyfm@gmail.com>
2014-06-06 21:24:07 -0400

    add sort and cache buster

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -3,17 +3,25 @@ require 'csv'
 class ExportController < ApplicationController
 	Time::DATE_FORMATS[:csv] = lambda { |time| time.strftime("%d/%m/%Y %I:%M%p") }
 	UTC_OFFSET = -4
+
+	before_filter :set_cache_buster
+
+	def set_cache_buster
+		response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+		response.headers["Pragma"] = "no-cache"
+		response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+	end
 	
 	def index
 		if params[:start] && params[:end]
 			start = Time.at(params[:start].to_i / 1000).to_datetime
 			_end = Time.at(params[:end].to_i / 1000).to_datetime
-			entries = Entry.where(created_at: (start.._end))
+			entries = Entry.where(created_at: (start.._end)).order(start: :asc)
 		else
-			entries = Entry.where(paid: false)
+			entries = Entry.where(paid: false).order(start: :asc)
 		end
 
-		offset = params[:offset].to_i || UTC_OFFSET
+		offset = params[:offset].to_i.minute || UTC_OFFSET.hour
 		
 
 		csv_string = CSV.generate do |csv|
@@ -21,7 +29,7 @@ class ExportController < ApplicationController
 		  hours = 0
 		  entries.each do |entry|
 		  	hour = ((entry.end - entry.start) / 1.hour).round
-		  	csv << [entry.description, (entry.start + offset.minute).to_formatted_s(:csv), (entry.end + offset.minute).to_formatted_s(:csv), hour]
+		  	csv << [entry.description, (entry.start + offset).to_formatted_s(:csv), (entry.end + offset).to_formatted_s(:csv), hour]
 		  	hours += hour
 		  end
 		  csv << ['Total', nil, nil, hours]
